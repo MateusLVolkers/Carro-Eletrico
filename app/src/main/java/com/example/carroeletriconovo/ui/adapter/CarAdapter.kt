4e08ea3f021d2e6891c3d1e824bfbcab464a8beb
trancosoWiliam <wiliam.trancoso@stone.com.br>
2023-02-07 21:08:35 -0300

    migracao do caradapter para viewbinding

--- app/src/main/java/com/example/carroeletriconovo/ui/adapter/CarAdapter.kt
+++ app/src/main/java/com/example/carroeletriconovo/ui/adapter/CarAdapter.kt
@@ -7,63 +7,56 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carroeletriconovo.R
+import com.example.carroeletriconovo.databinding.CarroRvBinding
 import com.example.carroeletriconovo.domain.Carro
 
-class CarAdapter(val carros: List<Carro>, val isFavoriteScreen: Boolean = false) : RecyclerView.Adapter<CarAdapter.ViewHolder>() {
+class CarAdapter(val carros: List<Carro>, val isFavoriteScreen: Boolean = false) :
+    RecyclerView.Adapter<CarAdapter.ViewHolder>() {
 
     var carItemLister: (Carro) -> Unit = {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.carro_rv, parent, false)
-        return ViewHolder(view)
+        val binding = CarroRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
     }
 
     //Troca a informação da view pela informação do item da lista de acordo com a posição
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.preco.text = carros[position].preco
-        holder.bateria.text = carros[position].bateria
-        holder.recarga.text = carros[position].recarga
-        holder.potencia.text = carros[position].potencia
-
-        if (isFavoriteScreen){
-            holder.favorito.setImageResource(R.drawable.ic_baseline_star_selected_24)
-        }
-
-        holder.favorito.setOnClickListener{
-            val carro = carros[position]
-            carItemLister(carro)
-            setupFavorito(carro, holder)
-        }
-    }
-
-    private fun setupFavorito(carro: Carro, holder: ViewHolder) {
-        carro.isFavorite = !carro.isFavorite
-
-        if (carro.isFavorite) {
-            holder.favorito.setImageResource(R.drawable.ic_baseline_star_selected_24)
-        } else {
-            holder.favorito.setImageResource(R.drawable.ic_baseline_star_border_24)
-        }
+        holder.render(carros[position])
     }
 
     //Qtd total de itens na lista
     override fun getItemCount(): Int = carros.size
 
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val preco: TextView
-        val bateria: TextView
-        val recarga: TextView
-        val potencia: TextView
-        val favorito: ImageView
+    inner class ViewHolder(val binding: CarroRvBinding) : RecyclerView.ViewHolder(binding.root) {
 
         init {
-            view.apply {
-                preco = findViewById(R.id.txt_valor_preco_id)
-                bateria = findViewById(R.id.bateria_valor_id)
-                recarga = findViewById(R.id.recarga_valor_id)
-                potencia = findViewById(R.id.potencia_valor_id)
-                favorito = findViewById(R.id.star_fav_id)
+            binding.starFavId.setOnClickListener {
+                val carro = carros[adapterPosition]
+                carItemLister(carro)
+                setupFavorito(carro)
+            }
+        }
+
+        fun render(carro: Carro) {
+            binding.txtValorPrecoId.text = carro.preco
+            binding.bateriaValorId.text = carro.bateria
+            binding.recargaValorId.text = carro.recarga
+            binding.potenciaValorId.text = carro.potencia
+
+            if (carro.isFavorite) {
+                binding.starFavId.setImageResource(R.drawable.ic_baseline_star_selected_24)
+            }
+        }
+
+        private fun setupFavorito(carro: Carro) {
+            carro.isFavorite = !carro.isFavorite
+
+            if (carro.isFavorite) {
+                binding.starFavId.setImageResource(R.drawable.ic_baseline_star_selected_24)
+            } else {
+                binding.starFavId.setImageResource(R.drawable.ic_baseline_star_border_24)
             }
         }
     }
